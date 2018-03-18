d24929ebb52863ca96b3ddee444a388c5f1fb5e5
zc <zachcaceres@gmail.com>
2018-03-18 12:04:12 -0400

    refactor: condense bookmarks logic with PromiseFactory

--- src/bookmarks/index.js
+++ src/bookmarks/index.js
@@ -1,4 +1,5 @@
 /* global chrome browser */
+import { PromiseFactory } from '../utils';
 
 /**
  * Retrieves BookmarkTreeNode(s) by id.
@@ -7,17 +8,7 @@
  * @return {Promise<Array<BookmarkTreeNode>>} Promise resolved with an array of [BookMarkTreeNode](https://developer.chrome.com/extensions/bookmarks#type-BookmarkTreeNode) or rejected with an error.
  */
 function getById(idOrIdList) {
-  if (chrome) {
-    return new Promise((resolve, reject) => {
-      chrome.bookmarks.get(idOrIdList, function(bookmarkTreeNodes) {
-        const err = chrome.runtime.lastError;
-        if (err) return reject(err);
-        resolve(bookmarkTreeNodes);
-      });
-    });
-  } else {
-    return browser.bookmark.get(idOrIdList);
-  }
+  return PromiseFactory(chrome.bookmarks.get, browser.bookmarks.get, idOrIdList);
 }
 
 /**
@@ -28,18 +19,7 @@ function getById(idOrIdList) {
  * @return {Promise<Array<BookMarkTreeNode>>}   Promise resolved with an array of BookmarkTreeNode or rejected with an error.
  */
 function getChildrenById(id) {
-  if (chrome) {
-    return new Promise((resolve, reject) => {
-      chrome.bookmarks.getChildren(function(bookmarkTreeNodes) {
-        const err = chrome.runtime.lastError;
-        if (err) return reject(err);
-        resolve(bookmarkTreeNodes);
-      });
-    });
-  } else {
-    return browser.bookmarks.getChildrenById(id);
-  }
-
+  return PromiseFactory(chrome.bookmarks.getChildren, browser.bookmarks.getChildren, id);
 }
 
 // function getRecent() {
